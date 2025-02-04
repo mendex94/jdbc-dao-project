70cd8bc2971f87aaf3cc9fc13bb580c18c00d91d
Felipe Mendes <lfelipecmendes@icloud.com>
2025-02-03 21:58:21 -0300

    feat: add `findById` method in `SellerDaoJDBC`

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,15 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Program {
 		public static void main(String[] args) {
-				System.out.println("Hello, World!");
+				SellerDao sellerDao = DaoFactory.createSellerDao();
+
+				Seller seller = sellerDao.findById(3);
+
+				System.out.println(seller);
 		}
 }
